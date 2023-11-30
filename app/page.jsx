828c81afea2114ac26e51b2947c32747f6e59f43
Aleksey-Dr <aprel181818@gmail.com>
2023-11-30 21:32:46 +0200

    About Us section
    About Us section of Home page

--- app/page.jsx
+++ app/page.jsx
@@ -3,6 +3,7 @@ import Container from "@/components/Container/Container";
 import PageTitle from "@/components/PageTitle/PageTitle";
 import Paragraph from "@/components/Paragraph/Paragraph";
 import MainHero from "@/components/MainHero/MainHero";
+import AboutUsSection from "@/components/HomePageComponents/AboutUsSection/AboutUsSection";
 
 export const metadata = {
   title: "G2S",
@@ -13,6 +14,9 @@ const HomePage = () => {
   return (
     <>
       <MainHero />
+      <Section>
+        <AboutUsSection />
+      </Section>
       <Section>
         <Container>
           <PageTitle text="Home Page" />
